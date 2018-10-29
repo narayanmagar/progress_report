4abcd39a6c50ed681b50ac6674d21998507886fd
narayanmagar <thmgrnarayan@gmail.com>
2018-10-29 22:42:26 +0545

    adding static files

--- marksheet/views.py
+++ marksheet/views.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.views.generic import ListView, TemplateView, DetailView
+from django.shortcuts import render, get_object_or_404
+
+from django.views.generic import View
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Hello, World!')
+class DashboardView(TemplateView):
+
+    template_name = "marksheet/dashboard.html"
+
+   
--- progress_report/urls.py
+++ progress_report/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-
-from marksheet import views
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from marksheet.views import DashboardView
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
-    ]
+    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += url(r'^$', DashboardView.as_view(), name='dashboard'),
